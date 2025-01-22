4718085e7120c27d574cd4d86421c8cc587ce391
FriedCoconut64 <vansterapples@gmail.com>
2025-01-22 10:25:47 -0600

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+Program #1; Hello World, Donovan Thompson 1/22/25
+
+def hello_world():
+
+    print("Hello World")
+
+hello_world()
+--------------
+Hello World
+
+Process finished with exit code 0
